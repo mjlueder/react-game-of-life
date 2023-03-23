86118690a486f51881b2148e7c638c6ba05b9178
Mary Lueder <mlueder@gmail.com>
2023-03-22 23:08:53 -0400

    add cell component and map cells

---
+++ src/Cell.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Game.css'
+
+const CELL_SIZE = 20
+// const WIDTH = 800, HEIGHT = 600
+
+class Cell extends React.Component {  
+  render() {    
+    const { x, y } = this.props;    
+    return (      
+    <div 
+      className="Cell" 
+      style={{        
+        left: `${CELL_SIZE * x + 1}px`,        
+        top: `${CELL_SIZE * y + 1}px`,        
+        width: `${CELL_SIZE - 1}px`,        
+        height: `${CELL_SIZE - 1}px`,      
+      }} 
+    />    
+    );  
+  }
+}
+
+export default Cell
--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Game.css'
+import Cell from "./Cell";
 
 const CELL_SIZE = 20, WIDTH = 800, HEIGHT = 600
 
@@ -61,16 +62,25 @@ class Game extends React.Component {
     this.setState({ cells: this.makeCells() });  
   }
   
+  
 
-  render() {    
+  render() {  
+    const { cells } = this.state;  
     return (      
       <div>        
         <div 
           className="Board"    
-          style={{ width: WIDTH, height: HEIGHT, backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}
+          style={{ 
+            width: WIDTH, 
+            height: HEIGHT, 
+            backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`
+          }}
           onClick={this.handleClick}
           ref={(n) => { this.boardRef = n; }}
-        >       
+        >
+          {cells.map(cell => (
+            <Cell x={cell.x} y={cell.y} key={`${cell.x},${cell.y}`}/>
+          ))}
         </div>      
       </div>    
     );  
