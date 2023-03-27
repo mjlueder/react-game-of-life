d2569d1e5e155092f5f1cfba6ace46b9e60ad2b3
Mary Lueder <mlueder@gmail.com>
2023-03-27 14:02:25 -0400

    create cell component

--- src/Cell.js
+++ src/Cell.js
@@ -1,24 +1,21 @@
-import React from 'react';
+// import React from 'react';
 import './Game.css'
 
 const CELL_SIZE = 20
-// const WIDTH = 800, HEIGHT = 600
+// switch to props
 
-class Cell extends React.Component {  
-  render() {    
-    const { x, y } = this.props;    
-    return (      
-    <div 
-      className="Cell" 
-      style={{        
-        left: `${CELL_SIZE * x + 1}px`,        
-        top: `${CELL_SIZE * y + 1}px`,        
-        width: `${CELL_SIZE - 1}px`,        
-        height: `${CELL_SIZE - 1}px`,      
-      }} 
-    />    
-    );  
-  }
+const Cell = (props) => {
+  const { x, y } = this.props
+  return (
+    <div className='Cell' style={{
+      left: `${CELL_SIZE * x + 1 }px`,
+      top: `${CELL_SIZE * y + 1 }px`,
+      width: `${CELL_SIZE - 1 }px`,
+      height: `${CELL_SIZE * x + 1 }px`,
+      backgroundColor: `${cellColors[Math.floor(Math.random()*6)]}`
+    }}>
+    </div>
+  )
 }
 
 export default Cell
