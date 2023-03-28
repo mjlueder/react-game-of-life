8aa785bc65c54861e12fd87d309e228c36855372
Mary Lueder <mlueder@gmail.com>
2023-03-28 15:06:52 -0400

    run iteration function

--- src/Game.js
+++ src/Game.js
@@ -100,6 +100,31 @@ class Game extends React.Component {
     }
   }
 
-  
+  runIteration() {
+    let newBoard = this.makeEmptyBoard()
+
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        let neighbors = this.calculateNeighbors(this.board, x, y)
+        if(this.board[y][x]) {
+          if (neighbors === 2 || neighbors === 3) {
+            newBoard[y][x] = true
+          } else {
+            newBoard[y][x] = false
+          }
+        } else {
+          if (!this.board[y][x] && neighbors === 3) {
+            newBoard[y][x] = true
+          }
+        }
+      }
+    }
+    this.board = newBoard
+    this.setState({ cells: this.makeCells() })
+
+    this.timeoutHandler = window.setTimeout(() => {
+      this.runIteration()
+    }, this.state.interval)
+  }
 
 }
